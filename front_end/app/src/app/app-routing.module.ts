2d848346960366a5c4b28cc66f9fb299334c2aa0
Ihor Bordakov <i.bordakov>
2020-03-17 23:25:42 +0200

    created Angular app, created Login and Registration components, started to code Registration template

---
+++ front_end/app/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { RegistrationComponent } from './components/registration/registration.component';
+import { LoginComponent } from './components/login/login.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/registration',
+    pathMatch: 'full'
+  },
+  {
+    path: 'registration',
+    component: RegistrationComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ front_end/app/src/app/models/user.model.ts
@@ -0,0 +1,11 @@
+export interface User {
+    id: number | string; //  should not be set up manually
+    email: string;
+    password: string;
+    name: string;
+    surname: string;
+    birthday: Date;
+    level: number; // should be calculated based on sales number; 0 after user registration
+    company: string;
+    rate: number;
+}
