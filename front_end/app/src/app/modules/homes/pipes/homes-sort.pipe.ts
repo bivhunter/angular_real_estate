44c24189a228cf2e5ade22f5e889cc00fef8a8a4
Ihor Bordakov <i.bordakov@soft-industry.com>
2020-03-31 13:35:55 +0300

    TRFEJS-73 completed implementation of HomesSortPipe

--- front_end/app/src/app/modules/homes/pipes/homes-sort.pipe.ts
+++ front_end/app/src/app/modules/homes/pipes/homes-sort.pipe.ts
@@ -16,19 +16,47 @@ export class HomesSortPipe implements PipeTransform {
 
 }
 
+function compareHome(homeA: Home, homeB: Home): number {
+  const compare = parseInt(homeA.home, 10) - parseInt(homeB.home, 10);
+  if (!compare) {
+    return homeA.home.slice(-1).localeCompare(homeB.home.slice(-1));
+  }
+  return compare;
+}
+
+function compareStreet(homeA: Home, homeB: Home): number {
+  return homeA.street.localeCompare(homeB.street);
+}
+
+function compareCity(homeA: Home, homeB: Home): number {
+  return homeA.city.localeCompare(homeB.city);
+}
+
+function compareState(homeA: Home, homeB: Home): number {
+  return homeA.state.localeCompare(homeB.state);
+}
+
+function comparePrice(homeA: Home, homeB: Home): number {
+  return homeB.price - homeA.price;
+}
+
+function compareHomes(homeA: Home, homeB: Home, ...funcArray: ((a: any, b: any) => number)[]): number {
+  if (!funcArray.length) {
+    return;
+  }
+  let count = 0;
+  let compare = funcArray[count](homeA, homeB);
+  while (!compare && funcArray[count + 1] ) {
+    compare = funcArray[++ count](homeA, homeB);
+  }
+  return compare;
+}
+
 function createSortFunc(): any {
   return {
     STREET_UP: (homes: Home[]) => {
       return [...homes].sort( (homeA, homeB) => {
-        const compare = homeA.street.localeCompare(homeB.street);
-        if (!compare) {
-          const compare2 = parseInt(homeA.home, 10) - parseInt(homeB.home, 10);
-          if (!compare2) {
-            return homeA.home.slice(-1).localeCompare(homeB.home.slice(-1));
-          }
-          return compare2;
-        }
-        return compare;
+        return compareHomes(homeA, homeB, compareStreet, compareCity, compareState, compareHome);
       });
     },
     STREET_DOWN: (homes: Home[]) => {
@@ -36,49 +64,36 @@ function createSortFunc(): any {
     },
     HOME_UP: (homes: Home[]) => {
       return [...homes].sort( (homeA, homeB) => {
-        const compare = parseInt(homeA.home, 10) - parseInt(homeB.home, 10);
-        if (!compare) {
-          const compare2 = homeA.home.slice(-1).localeCompare(homeB.home.slice(-1));
-          if (!compare2) {
-            return homeA.street.localeCompare(homeB.street);
-          }
-          return compare2;
-        }
-        return compare;
+        return compareHomes(homeA, homeB, compareHome, compareStreet, compareCity, compareState);
       });
     },
     HOME_DOWN: (homes: Home[]) => {
       return createSortFunc().HOME_UP(homes).reverse();
     },
     CITY_UP: (homes: Home[]) => {
-      return [...homes].sort( (homeA, homeB) => {
-        return homeA.city.localeCompare(homeB.city);
-      });
+      return [...homes]
+        .sort( (homeA, homeB) => {
+          return compareHomes(homeA, homeB, compareCity, compareState, compareStreet, compareHome);
+        });
     },
     CITY_DOWN: (homes: Home[]) => {
-      return [...homes].sort( (homeA, homeB) => {
-        return homeB.city.localeCompare(homeA.city);
-      });
+      return createSortFunc().CITY_UP(homes).reverse();
     },
     STATE_UP: (homes: Home[]) => {
       return [...homes].sort( (homeA, homeB) => {
-        return homeA.state.localeCompare(homeB.state);
+        return compareHomes(homeA, homeB, compareState, compareCity,  compareStreet, compareHome);
       });
     },
     STATE_DOWN: (homes: Home[]) => {
-      return [...homes].sort( (homeA, homeB) => {
-        return homeB.state.localeCompare(homeA.state);
-      });
+      return createSortFunc().STATE_UP(homes).reverse();
     },
     PRICE_UP: (homes: Home[]) => {
       return [...homes].sort( (homeA, homeB) => {
-        return homeB.price - homeA.price;
+        return compareHomes(homeA, homeB, comparePrice, compareState, compareCity,  compareStreet, compareHome);
       });
     },
     PRICE_DOWN: (homes: Home[]) => {
-      return [...homes].sort( (homeA, homeB) => {
-        return homeA.price - homeB.price;
-      });
+      return createSortFunc().PRICE_UP(homes).reverse();
     }
   };
 }
